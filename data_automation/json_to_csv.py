3950660fdfdbd434884d74ee7627917f54e420d4
Duc Tuyen Ta <tuyentd86@gmail.com>
2022-08-09 16:36:03 +0200

    python code to convert json to csv

---
+++ data_automation/json_to_csv.py
@@ -0,0 +1,14 @@
+" Decode JSON into a beautiful structured CSV "
+import json, csv
+
+with open("data.json") as file:
+    data = json.load(file)
+
+emp_details = data['emp_details']
+
+
+with open("data.csv", 'w', newline='') as f:
+    csv_writer = csv.writer(f)
+    csv_writer.writerow(emp_details[0].keys())
+    for row in emp_details:
+        csv_writer.writerow(row.values())
